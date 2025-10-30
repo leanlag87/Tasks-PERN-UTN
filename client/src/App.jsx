a673013229ec225515ea63cf75e7f0f7f30a41ef
Leandro Gonzalez <leanlag@gmail.com>
2025-10-29 21:08:33 -0300

    refactor: eliminar código comentado en el componente App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,61 +13,6 @@ import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
 import { useAuth } from "./context/AuthContext.jsx";
 import { TasksProvider } from "./context/TasksContext.jsx";
 
-// const App = () => {
-//   const { isAuth, loading } = useAuth();
-//   if (loading) {
-//     return <h1>Cargando...</h1>;
-//   }
-
-//   return (
-//     <>
-//       <Navbar />
-//       <Container className="py-5">
-//         <Routes>
-//           {/* Rutas públicas accesibles solo si no está autenticado "!isAuth"
-//            Si el usuario está autenticado, será redirigido a la página de tareas
-//             Usamos la propiedad redirectTo para definir la ruta de redirección*/}
-//           <Route
-//             element={<ProtectedRoute isAllowed={!isAuth} redirectTo="/tasks" />}
-//           >
-//             <Route path="/" element={<HomePage />} />
-//             <Route path="/about" element={<AboutPage />} />
-//             <Route path="/login" element={<LoginPage />} />
-//             <Route path="/register" element={<RegisterPage />} />
-//           </Route>
-
-//           {/* Rutas protegidas accesibles solo si está autenticado "isAuth"
-//            Si el usuario no está autenticado, será redirigido a la página de inicio de sesión
-//            Usamos la propiedad redirectTo para definir la ruta de redirección */}
-//           <Route
-//             element={<ProtectedRoute isAllowed={isAuth} redirectTo="/login" />}
-//           >
-//             <Route path="/profile" element={<ProfilePage />} />
-
-//             {/*Rutas de tareas protegidas dentro del TasksProvider
-//             Proporciona el contexto de tareas a todos los componentes hijos
-//             solo se accede a estas rutas si el usuario está autenticado*/}
-
-//             <Route
-//               element={
-//                 <TasksProvider>
-//                   <Outlet />
-//                 </TasksProvider>
-//               }
-//             >
-//               <Route path="/tasks" element={<TasksPage />} />
-//               <Route path="/tasks/new" element={<TaskFormPage />} />
-//               <Route path="/tasks/edit/:id" element={<TaskFormPage />} />
-//             </Route>
-//           </Route>
-
-//           <Route path="*" element={<NotFound />} />
-//         </Routes>
-//       </Container>
-//     </>
-//   );
-// };
-
 function App() {
   const { loading } = useAuth();
 
